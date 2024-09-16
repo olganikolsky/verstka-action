7e8445921783f4cd68d4e03981b8daf9fa5878ca
Olga <olganikolsky@inbox.ru>
2024-09-16 22:21:22 +0300

    first commit

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,20 +1,30 @@
 import './style.css';
+let whitebox = document.querySelector('.whitebox');
 
 let but = document.querySelector('.button');
 but.addEventListener('click', function () {
-  document.body.innerHTML += `<div class = "rightbox">
-    <p class="head3">Total:</p>
-    <div class="row">
-    <p class="head4">Gabrielle Essence<br> Eau De Parfum</p>
-    <p class="price1">$149.99</p> 
-    <p class="tickets">1</p> 
-    <p class="price2">$149.99</p> 
-  </div>
-    <p class="price3">$149.99</p> 
-  </div>`;
+  whitebox.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="rightbox">
+      <p class="head3">Total:</p>
+      <div class="row">
+        <p class="head4">Gabrielle Essence<br> Eau De Parfum</p>
+        <p class="price1">$149.99</p> 
+        <p class="tickets">1</p> 
+        <p class="price2">$149.99</p> 
+      </div>
+      <p class="price3">$149.99</p> 
+    </div>
+  `
+  );
 });
 
 let nowpice = document.querySelector('.now');
-button.addEventListener('click', function () {
+let priceHigh = document.querySelector('.price3');
+
+but.addEventListener('click', function () {
   nowpice.style.color = 'grey';
+  but.style.background = 'grey';
+  priceHigh.style.color = 'grey';
 });
