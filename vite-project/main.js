76b42803556a58e9c88ee7ea9b676eb4a14aed32
Olga <olganikolsky@inbox.ru>
2024-09-16 20:28:16 +0300

    first commit

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,25 +1,7 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-let button = document.querySelector('.button');
-button.addEventListener('click', function () {
+let but = document.querySelector('.button');
+but.addEventListener('click', function () {
   document.body.innerHTML += `<div class = "rightbox">
     <p class="head3">Total:</p>
     <div class="row">
@@ -30,4 +12,9 @@ button.addEventListener('click', function () {
   </div>
     <p class="price3">$149.99</p> 
   </div>`;
+});
+
+let nowpice = document.querySelector('.now');
+button.addEventListener('click', function () {
+  nowpice.style.color = 'grey';
 });
